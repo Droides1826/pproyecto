f41bc694801791fb95faa3cd29dabfb5078f7cfd
Droides1826 <jesus_dgonzalez@soy.sena.co>
2025-01-24 16:36:05 -0500

    se agregaron las api de consulta de las demas vistas

--- routes/categorias.py
+++ routes/categorias.py
@@ -15,15 +15,62 @@ def mostrar_categorias():
         categorias.append(categoria)
     return jsonify(categorias) 
 
-@categorias.route('/ferreteria', methods=['GET'])
-def ferreteria():
+@categorias.route('/hogar', methods=['GET'])
+def hogar():
     MySQL = conexion.connection.cursor()
-    MySQL.execute('SELECT * FROM `categorias` WHERE nombre_categoria = "Ferreteria";')
+    MySQL.execute('SELECT * FROM `productos` WHERE id_categoria = 1;')
     data = MySQL.fetchall()
     columnas = [desc[0] for desc in MySQL.description]
     categorias = []
     for row in data:
         categoria = dict(zip(columnas, row))
         categorias.append(categoria)
-    return render_template('ferreteria.html', data=categorias)
-    
+    return jsonify(categorias)
+
+@categorias.route('/comida', methods=['GET'])
+def comida():
+    MySQL = conexion.connection.cursor()
+    MySQL.execute('SELECT * FROM `productos` WHERE id_categoria = 2;')
+    data = MySQL.fetchall()
+    columnas = [desc[0] for desc in MySQL.description]
+    categorias = []
+    for row in data:
+        categoria = dict(zip(columnas, row))
+        categorias.append(categoria)
+    return jsonify(categorias)
+
+@categorias.route('/mascotas', methods=['GET'])
+def mascotas():
+    MySQL = conexion.connection.cursor()
+    MySQL.execute('SELECT * FROM `productos` WHERE id_categoria = 3;')
+    data = MySQL.fetchall()
+    columnas = [desc[0] for desc in MySQL.description]
+    categorias = []
+    for row in data:
+        categoria = dict(zip(columnas, row))
+        categorias.append(categoria)
+    return jsonify(categorias)
+
+@categorias.route('/vehiculos', methods=['GET'])
+def vehiculos():
+    MySQL = conexion.connection.cursor()
+    MySQL.execute('SELECT * FROM `productos` WHERE id_categoria = 4;')
+    data = MySQL.fetchall()
+    columnas = [desc[0] for desc in MySQL.description]
+    categorias = []
+    for row in data:
+        categoria = dict(zip(columnas, row))
+        categorias.append(categoria)
+    return jsonify(categorias)
+
+@categorias.route('/ferreteria', methods=['GET'])
+def ferreteria():
+    MySQL = conexion.connection.cursor()
+    MySQL.execute('SELECT * FROM `productos` WHERE id_categoria = 5;')
+    data = MySQL.fetchall()
+    columnas = [desc[0] for desc in MySQL.description]
+    categorias = []
+    for row in data:
+        categoria = dict(zip(columnas, row))
+        categorias.append(categoria)
+    return jsonify(categorias)
