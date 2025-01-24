4dde19e831d101ced8913c19a64ee7d4698c2753
Droides1826 <jesus_dgonzalez@soy.sena.co>
2025-01-24 10:57:09 -0500

    se le implementaron imagenes a los productos

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask
 from utils.db import conexion
 from routes.productos import productos
 from routes.categorias import categorias
+import os
 
 app = Flask(__name__, template_folder='template')
 
@@ -12,29 +13,13 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pproyecto'
 conexion.init_app(app)
 
-@app.route('/')
-def index():
-    MySQL = conexion.connection.cursor()
-    MySQL.execute('SELECT p.id_producto, p.nombre, p.descripcion FROM productos p')
-    data = MySQL.fetchall()
-    columnas = [desc[0] for desc in MySQL.description]
-    productos = []
-    for row in data:
-        producto = dict(zip(columnas, row))
-        productos.append(producto)
-    return render_template('index.html', data=productos)
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'jfif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/productos_por_categoria/<int:id_categoria>', methods=['GET'])
-def productos_por_categoria(id_categoria):
-    MySQL = conexion.connection.cursor()
-    MySQL.execute('SELECT p.id_producto, p.nombre, p.descripcion, p.precio, p.estado, c.nombre_categoria, p.cantidad FROM productos p JOIN categorias c ON p.id_categoria = c.id_categoria WHERE c.id_categoria = %s;', (id_categoria,))
-    data = MySQL.fetchall()
-    columnas = [desc[0] for desc in MySQL.description]
-    productos = []
-    for row in data:
-        producto = dict(zip(columnas, row))
-        productos.append(producto)
-    return jsonify(productos)
-
+# Crear la carpeta de uploads si no existe
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+    
 app.register_blueprint(productos)
 app.register_blueprint(categorias)
